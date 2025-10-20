dcb8e953c67e0cffc1a5d3d1e5674131cce0f56e
Omkar Shelke <85251955+omkar-shelke25@users.noreply.github.com>
2025-10-20 19:20:11 +0530

    Create background.sh

---
+++ background.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Create namespace
+kubectl create namespace headlamp
+
+# Add Headlamp repo
+helm repo add headlamp https://headlamp-k8s.github.io/headlamp/
+helm repo update
+
+# Install Headlamp with NodePort
+helm install headlamp headlamp/headlamp \
+  --namespace headlamp \
+  --set service.type=NodePort \
+  --set service.nodePort=30080
+
+# Wait for deployment
+kubectl -n headlamp rollout status deploy/headlamp --timeout=120s
+
+# Create and save token
+kubectl create token headlamp -n headlamp > /root/headlamp-token
+
+echo "Headlamp installed successfully"
